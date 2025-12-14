245ed30152013d8e588cf5eed74ce8aa8e088459
santhosh17august2003-cloud <santhosh17august2003@gmail.com>
2025-12-14 15:50:57 +0530

    upload files

--- db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["robot_db"]          # database name
-collection = db["users"]         # collection name
