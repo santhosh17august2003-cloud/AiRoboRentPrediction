0dd910c2ae29675d486c4d4c97d4b08f9b1a8eec
santhosh17august2003-cloud <santhosh17august2003@gmail.com>
2026-09-02 20:09:22 +0530

    Remove legacy Vercel serverless functions from frontend

--- myfrontend/api/health.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-from _response import send_json
-
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        send_json(self, {"status": "ok"})
